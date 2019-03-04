bec3d022e0f183a055a1d61593db73ded457355e
Josh Date <jjdate@live.com>
2019-03-04 17:41:01 -0500

    done but will need to start over cuz i cannot import parent file in views

---
+++ photogur/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from photogur.models import Picture, Comment
+
+admin.site.register(Picture)
+
+admin.site.register(Comment)
+	
---
+++ photogur/views.py
@@ -0,0 +1,8 @@
+#from django.shortcuts import render
+#from django.http import HttpResponse
+#from django.urls import path
+
+#def pictures(request):
+#	return HttpResponse('pictures/')
+
+#Parent Folder is cannot be read. Views.py will be merged with urls.py.
